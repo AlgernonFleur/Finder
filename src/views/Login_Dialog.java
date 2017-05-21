c9acd03a829013360281c5f21b425a40a925a449
Algernon <algernon.fleur@gmail.com>
2017-05-21 21:17:58 +1000

    Setup login view

--- src/views/Login_Dialog.java
+++ src/views/Login_Dialog.java
@@ -1,4 +1,18 @@
 package views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Login_Dialog {
+	
+	@FXML private TextField usernameField;
+	@FXML private TextField passwordField;
+	@FXML private Button loginButton;
+	@FXML private Text loginFailState;
+	
+	@FXML private void initialize(){
+	
+	}
 }
