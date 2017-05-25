5ece977486bafbd9b860350a1e8921929b3050b7
Jackmcclatchey <jackmcclatchey@Jacks-MacBook-Pro.local>
2017-05-26 02:02:01 +1000

    some search functions

--- src/fff/util/Search.java
+++ src/fff/util/Search.java
@@ -1,5 +1,80 @@
 package fff.util;
 
+import fff.models.Restaurant;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Search {
 
+	
+	public ObservableList<Restaurant> getSearchResultsName(ObservableList<Restaurant> restaurants, String searchInput) {
+		
+		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
+		
+		int i = 0;
+
+		while (i < restaurants.size()) {
+			if (restaurants.get(i).getName().contains(searchInput)) {
+				searchResults.add(restaurants.get(i));
+			}
+			i++;
+		}
+		return searchResults;
+	}
+
+
+	public ObservableList<Restaurant> getSearchResultsPostcode(ObservableList<Restaurant> restaurants, String searchInput) {
+		
+		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
+		
+		int i = 0;
+		
+		while (i < restaurants.size()) {
+			if (Integer.toString(restaurants.get(i).getPostcode()).contains(searchInput)) {
+				searchResults.add(restaurants.get(i));
+			}
+			i++;
+		}
+		return searchResults;
+	}
+	
+	
+	public ObservableList<Restaurant> getSearchResultsCuisine(ObservableList<Restaurant> restaurants, String searchInput) {
+		
+		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
+		
+		int i = 0;
+		
+		while (i < restaurants.size()) {
+			if (restaurants.get(i).getCuisine().contains(searchInput)) {
+				searchResults.add(restaurants.get(i));
+			}
+			i++;
+		}
+		return searchResults;
+	}
+	
+	
+	public ObservableList<Restaurant> getSearchResultsRatings(ObservableList<Restaurant> restaurants, String searchInput) {
+		
+		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
+		
+		int i = 0;
+		
+		float inputRating = Float.MAX_VALUE;
+		try {
+			inputRating = Float.parseFloat(searchInput);
+		} catch (NumberFormatException e) {
+			
+		}
+		
+		while (i < restaurants.size()) {
+			if (restaurants.get(i).getAverageRatings() >= inputRating) {
+				searchResults.add(restaurants.get(i));
+			}
+			i++;
+		}
+		return searchResults;
+	}
+	
 }
