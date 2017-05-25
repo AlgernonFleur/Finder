03b28faf7ddd5a800c0aa8c84b8bd739675f7311
Algernon <algernon.fleur@gmail.com>
2017-05-25 19:44:33 +1000

    Testing search Page

---
+++ src/fff/views/Search_Page.java
@@ -0,0 +1,37 @@
+package fff.views;
+
+import fff.models.Restaurant;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class Search_Page extends _View_ {
+	
+	@FXML private TextField searchBar;
+	@FXML private Button searchButton;
+	@FXML private ComboBox<String> searchOptions;
+	@FXML private TableView<Restaurant> searchResults;
+	
+	@FXML public void initialize(){
+		this.searchBar.setPromptText("Search restaurants");
+		this.searchBar.setOnKeyPressed(e-> {
+			if(e.getCode().getName().equals("Enter"))search();
+		});
+		this.searchButton.setOnAction(e->search());
+		
+		ObservableList<String> options = FXCollections.observableArrayList(
+			"Name","Postcode","Cuisine","Ratings","Price Range");
+		this.searchOptions.setItems(options);
+		this.searchOptions.getSelectionModel().selectFirst();
+	}
+	
+	private void search(){
+		if(!searchBar.getText().equals("")){
+			System.out.println(searchBar.getText()+" "+searchOptions.getValue());
+		}
+	}
+}
