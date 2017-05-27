d95fe0ad6e1b40217171112451b9069132c2b774
Algernon <algernon.fleur@gmail.com>
2017-05-27 17:16:48 +1000

    Updated Search page

--- src/fff/util/Search.java
+++ src/fff/util/Search.java
@@ -5,14 +5,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public final class Search {
-
 	
 	public static ObservableList<Restaurant> getSearchResultsName(ObservableList<Restaurant> restaurants, String searchInput) {
-		
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
-
 		while (i < restaurants.size()) {
 			if (restaurants.get(i).getName().contains(searchInput)) {
 				searchResults.add(restaurants.get(i));
@@ -24,11 +21,9 @@ public final class Search {
 
 
 	public static ObservableList<Restaurant> getSearchResultsPostcode(ObservableList<Restaurant> restaurants, String searchInput) {
-		
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
-		
 		while (i < restaurants.size()) {
 			if (Integer.toString(restaurants.get(i).getPostcode()).contains(searchInput)) {
 				searchResults.add(restaurants.get(i));
@@ -40,11 +35,9 @@ public final class Search {
 	
 	
 	public static ObservableList<Restaurant> getSearchResultsCuisine(ObservableList<Restaurant> restaurants, String searchInput) {
-		
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
-		
 		while (i < restaurants.size()) {
 			if (restaurants.get(i).getCuisine().contains(searchInput)) {
 				searchResults.add(restaurants.get(i));
@@ -56,11 +49,9 @@ public final class Search {
 	
 	
 	public static ObservableList<Restaurant> getSearchResultsRatings(ObservableList<Restaurant> restaurants, String searchInput) {
-		
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
-		
 		float inputRating = Float.MAX_VALUE;
 		try {
 			inputRating = Float.parseFloat(searchInput);
