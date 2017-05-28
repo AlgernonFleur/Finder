6ce49a48c50d7e9317e9c770f49c48d8da08fd25
Algernon <algernon.fleur@gmail.com>
2017-05-28 19:28:23 +1000

    Update to search pt1

--- src/fff/util/Search.java
+++ src/fff/util/Search.java
@@ -6,49 +6,51 @@ import javafx.collections.ObservableList;
 
 public final class Search {
 	
-	public static ObservableList<Restaurant> getSearchResultsName(ObservableList<Restaurant> restaurants, String searchInput) {
+	public static ObservableList<Restaurant>
+	getSearchResultsName(ObservableList<Restaurant> restaurants, String searchInput) {
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
 		while (i < restaurants.size()) {
-			if (restaurants.get(i).getName().contains(searchInput)) {
+			if (restaurants.get(i).getName().contains(searchInput))
 				searchResults.add(restaurants.get(i));
-			}
 			i++;
 		}
 		return searchResults;
 	}
 
-
-	public static ObservableList<Restaurant> getSearchResultsPostcode(ObservableList<Restaurant> restaurants, String searchInput) {
+	
+	public static ObservableList<Restaurant>
+	getSearchResultsPostcode(ObservableList<Restaurant> restaurants, String searchInput) {
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
 		while (i < restaurants.size()) {
-			if (Integer.toString(restaurants.get(i).getPostcode()).contains(searchInput)) {
+			if (Integer.toString(restaurants.get(i).getPostcode()).contains(searchInput))
 				searchResults.add(restaurants.get(i));
-			}
 			i++;
 		}
 		return searchResults;
 	}
 	
 	
-	public static ObservableList<Restaurant> getSearchResultsCuisine(ObservableList<Restaurant> restaurants, String searchInput) {
+	public static ObservableList<Restaurant>
+	getSearchResultsCuisine(ObservableList<Restaurant> restaurants, String searchInput) {
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
 		while (i < restaurants.size()) {
-			if (restaurants.get(i).getCuisine().contains(searchInput)) {
+			if (restaurants.get(i).getCuisine().contains(searchInput))
 				searchResults.add(restaurants.get(i));
-			}
+			
 			i++;
 		}
 		return searchResults;
 	}
 	
 	
-	public static ObservableList<Restaurant> getSearchResultsRatings(ObservableList<Restaurant> restaurants, String searchInput) {
+	public static ObservableList<Restaurant>
+	getSearchResultsRatings(ObservableList<Restaurant> restaurants, String searchInput) {
 		ObservableList<Restaurant> searchResults = FXCollections.observableArrayList();
 		
 		int i = 0;
