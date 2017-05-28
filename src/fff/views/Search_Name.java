0953d4507e266ff40ed9417d8a3d52d3415fe117
Algernon <algernon.fleur@gmail.com>
2017-05-28 18:22:10 +1000

    Search by name seems to be done well

--- src/fff/views/Search_Name.java
+++ src/fff/views/Search_Name.java
@@ -1,7 +1,10 @@
 package fff.views;
 
 import fff.models.Restaurant;
+import fff.util.Search;
+import fff.util.Sort;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
@@ -22,6 +25,11 @@ public class Search_Name extends _View_ {
 	@FXML private Button backButton;
 	
 	@FXML public void initialize(){
+		resName.setSortable(false);
+		resCuisine.setSortable(false);
+		resPostcode.setSortable(false);
+		resRating.setSortable(false);
+		resPrice.setSortable(false);
 		searchBar.setOnKeyPressed(e->{
 			if(e.getCode().equals(KeyCode.ENTER))search();
 		});
@@ -33,10 +41,45 @@ public class Search_Name extends _View_ {
 			"Sort by Rating Asc.","Sort by Rating Des.",
 			"Sort by Avg. Price Asc.","Sort by Avg. Price Des."
 		));
+		sortOptions.setPromptText("Sort results");
+		sortOptions.setOnAction(e->sort());
 		backButton.setOnAction(e->getMain().changeCenter(getPreviousPage()));
 	}
 	
 	private void search(){
+		ObservableList<Restaurant> results =
+			Search.getSearchResultsName(_Overview_.getDatabase().getRestaurants(),searchBar.getText());
+		if(results.size()==0){
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("Results Empty");
+			alert.setHeaderText(null);
+			alert.setContentText("Your search query returned no results!");
+			alert.showAndWait();
+		}else{
+			sort();
+			resultsTableView.setItems(results);
+			resName.setCellValueFactory(d->d.getValue().nameProperty());
+			resCuisine.setCellValueFactory(d->d.getValue().cuisineProperty());
+			resPostcode.setCellValueFactory(d->d.getValue().postcodeProperty().asString());
+			resRating.setCellValueFactory(d->d.getValue().averageRatingsProperty().asString("%1.1f"));
+			resPrice.setCellValueFactory(d->d.getValue().averageRatingsProperty().asString("%2.2f"));
+		}
+	}
 	
+	private void sort(){
+		ObservableList<Restaurant> results = resultsTableView.getItems();
+		switch(sortOptions.getSelectionModel().getSelectedIndex()){
+			default:
+			case 0: results.sort(Sort.ResNameCompAsc);break;
+			case 1: results.sort(Sort.ResNameCompDes);break;
+			case 2: results.sort(Sort.ResCuisineCompAsc);break;
+			case 3: results.sort(Sort.ResCuisineCompDes);break;
+			case 4: results.sort(Sort.ResPostcodeCompAsc);break;
+			case 5: results.sort(Sort.ResPostcodeCompDes);break;
+			case 6: results.sort(Sort.ResRatingCompAsc);break;
+			case 7: results.sort(Sort.ResRatingCompDes);break;
+			case 8: results.sort(Sort.ResPriceCompAsc);break;
+			case 9: results.sort(Sort.ResPriceCompDes);break;
+		}
 	}
 }
